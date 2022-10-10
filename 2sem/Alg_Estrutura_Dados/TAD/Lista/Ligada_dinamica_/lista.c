0910500ce34e9b7dcc7d7e2af03b66fcf85922fd
Pedro pwdrc Tolvo <pwdrc@protonmail.com>
2022-10-10 17:53:26 -0300

    implementação lista ligada - funcao cria lista

---
+++ 2sem/Alg_Estrutura_Dados/TAD/Lista/Ligada_dinamica_/lista.c
@@ -0,0 +1,13 @@
+#include"lista.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+int criarLista(tipo_lista *l) {
+    l->inicio = NULL;
+}
+
+
+
+/**int inserir(tipo_lista *l, tipo_elemento e) {
+
+}
---
+++ 2sem/Alg_Estrutura_Dados/TAD/Lista/Ligada_dinamica_/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"lista.h"
+
+int main() {
+    tipo_lista l;
+    tipo_elemento e;
+    tipo_apontador p;
+
+    criar(&l);
+}
